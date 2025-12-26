d5985614b3eef6dd605c154e9a548f827822e918
Levi-K <anhnh1@eupgroup.net>
2025-12-26 15:33:27 +0700

    [HA] Try CI/CD

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,18 @@
-// Tauri doesn't have a Node.js server to do proper SSR
-// so we use adapter-static with a fallback to index.html to put the site in SPA mode
-// See: https://svelte.dev/docs/kit/single-page-apps
-// See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from '@sveltejs/adapter-static'; // Đổi từ adapter-auto sang adapter-static
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
-      fallback: "index.html",
-    }),
-  },
+      pages: 'build',  // Thư mục đầu ra phải khớp với "frontendDist" trong tauri.conf.json
+      assets: 'build',
+      fallback: 'index.html', // Quan trọng cho chế độ SPA (Single Page App)
+      precompress: false,
+      strict: true
+    })
+  }
 };
 
 export default config;
